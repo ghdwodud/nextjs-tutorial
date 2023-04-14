8bcd11a842f4cf5dbbd00625c82a8dfa790c58b9
jyhong <jyhong@podovat.com>
2023-04-14 22:25:53 +0900

    next js 스켈레톤 코드 추가

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Document from "./_document";
+export default function Home() {
+  return (
+    <div>
+      <Head>dfdf</Head>
+      
+      
+    </div>
+  )
+}
